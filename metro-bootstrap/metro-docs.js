12f997c9c8abe94fb577d8bb1aaa5d6989d9804b
Nelson Reis <nelsonfsr@gmail.com>
2013-04-05 00:16:32 +0100

    fix #19 add script to prevent scroll on button clicks (type=submit)

---
+++ metro-bootstrap/metro-docs.js
@@ -0,0 +1,16 @@
+/* =====================================================
+ * NOTE: THIS JAVASCRIPT IS ONLY TO BE USED IN OUR DOCS.
+ * DO NOT INCLUDE THIS ON YOUR APP.
+ * ===================================================== */
+
+!function ($) {
+
+  $(function () {
+
+    $(":submit").click(function (e) {
+		e.preventDefault()
+    })
+	
+  })
+
+}(window.jQuery);
