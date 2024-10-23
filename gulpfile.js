6a37140c0bed400cad706f00c5f218ebc5a65edd
Bashamega <adambashaahmednaji@gmail.com>
2024-10-23 09:50:19 +0300

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,117 +1,39 @@
-'use strict';
-
 const gulp = require('gulp');
 const less = require('gulp-less');
+const path = require('path');
 const clean = require('gulp-clean');
-const jshint = require('gulp-jshint');
-const connect = require('gulp-connect');
-const imagemin = require('gulp-imagemin');
-const svgmin = require('gulp-svgmin');
-const cssnano = require('gulp-cssnano');
-const htmlmin = require('gulp-htmlmin');
-const rev = require('gulp-rev');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
+const cssmin = require('gulp-cssnano');
 const plumber = require('gulp-plumber');
-const livereload = require('gulp-livereload');
 
-// Paths
+// Define paths
 const paths = {
-  app: 'app',
-  dist: 'dist',
-  docs: 'docs',
-  less: 'app/less/**/*.less',
-  html: 'app/*.html',
-  scripts: 'app/scripts/**/*.js',
-  images: 'app/images/**/*.{png,jpg,jpeg,gif,webp,svg}',
-  fonts: 'app/fonts/**/*.*',
-  bootstrap: 'app/bower_components/bootstrap/less/**/*',
-  faFonts: 'app/bower_components/font-awesome/fonts/**/*'
+  less: 'app/less/**/*.less', // Source LESS files
+  dist: 'dist/css/',          // Destination folder for compiled CSS
+  variables: 'app/less/variables.less', // Variables file
 };
 
-// Clean Task
-gulp.task('clean', () => {
-  return gulp.src([paths.dist, '.tmp'], { read: false, allowEmpty: true })
+// Task to compile LESS files
+gulp.task('less', function () {
+  return gulp.src(paths.less)
+    .pipe(plumber({
+      errorHandler: function (err) {
+        console.error('Error!', err.message);
+        this.emit('end');
+      }
+    }))
+    .pipe(less({
+      paths: [path.join(__dirname, 'app/less')],
+      // Import the variables.less file in the main LESS file
+    }))
+    .pipe(cssmin())
+    .pipe(gulp.dest(paths.dist));
+});
+
+// Task to clean the dist folder
+gulp.task('clean', function () {
+  return gulp.src('dist', { read: false, allowEmpty: true })
     .pipe(clean());
 });
 
-// LESS Task
-gulp.task('less', () => {
-  return gulp.src(`${paths.app}/less/**/*.less`)
-    .pipe(plumber())
-    .pipe(less({ sourceMap: true }))
-    .pipe(gulp.dest(`${paths.dist}/css`))
-    .pipe(connect.reload());
-});
-
-// JSHint Task
-gulp.task('jshint', () => {
-  return gulp.src([paths.scripts])
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'));
-});
-
-// Image Minification
-gulp.task('imagemin', () => {
-  return gulp.src(paths.images)
-    .pipe(imagemin())
-    .pipe(gulp.dest(`${paths.dist}/images`));
-});
-
-// SVG Minification
-gulp.task('svgmin', () => {
-  return gulp.src(`${paths.app}/images/**/*.svg`)
-    .pipe(svgmin())
-    .pipe(gulp.dest(`${paths.dist}/images`));
-});
-
-// CSS Minification
-gulp.task('cssmin', () => {
-  return gulp.src(`${paths.dist}/css/*.css`)
-    .pipe(cssnano())
-    .pipe(gulp.dest(`${paths.dist}/css`));
-});
-
-// HTML Minification
-gulp.task('htmlmin', () => {
-  return gulp.src(paths.html)
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest(paths.dist))
-    .pipe(connect.reload());
-});
-
-// Copy Task
-gulp.task('copy', () => {
-  return gulp.src([
-    '*.{ico,png,txt}',
-    `${paths.fonts}`,
-    `${paths.bootstrap}`,
-    `${paths.faFonts}`
-  ], { base: paths.app })
-    .pipe(gulp.dest(paths.dist))
-    .pipe(connect.reload());
-});
-
-// Live Reload Server
-gulp.task('connect', () => {
-  connect.server({
-    root: [paths.dist],
-    livereload: true,
-    port: 9000
-  });
-});
-
-// Watch Task
-gulp.task('watch', () => {
-  livereload.listen();
-  gulp.watch(paths.less, gulp.series('less'));
-  gulp.watch(paths.html, gulp.series('htmlmin'));
-  gulp.watch(paths.scripts, gulp.series('jshint'));
-  gulp.watch(paths.images, gulp.series('imagemin'));
-});
-
-// Build Task
-gulp.task('build', gulp.series('clean', gulp.parallel('less', 'imagemin', 'svgmin', 'htmlmin', 'copy')));
-
-// Default Task
-gulp.task('default', gulp.series('build', gulp.parallel('connect', 'watch')));
+// Default task to clean and compile LESS
+gulp.task('default', gulp.series('clean', 'less'));
